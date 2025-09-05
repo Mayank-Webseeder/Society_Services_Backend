bbdee22d7b43c2ca05f7f5d2d7caf08776cfb4a7
Vinayakk04 <vinayakksharma.14@gmail.com>
2025-09-05 12:35:47 +0530

    new cors

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const PORT = process.env.PORT || 5003;
 // ✅ CORS Configuration
 const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:5174",
   "https://social-services-app.vercel.app",
   "https://delightful-pastelito-988e6f.netlify.app",
   "https://admin.mysocietyneeds.com",
