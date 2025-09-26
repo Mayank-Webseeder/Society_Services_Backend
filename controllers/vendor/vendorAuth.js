e97aaa050561c84c4d5c7df8c95f51c5996534f0
MohitWadhwaniWebseeder <wadhwani.mohit@webseeder.tech>
2025-09-26 15:43:08 +0530

    Changes api req body for testing purpose

--- controllers/vendor/vendorAuth.js
+++ controllers/vendor/vendorAuth.js
@@ -198,6 +198,9 @@ exports.createVendorProfile = async (req, res) => {
 		if (req.body.uniqueName) {
 			updateData.idProof = "uploads/" + req.body.uniqueName;
 		}
+		if (req.body.paymentSuccess) {
+			updateData.isSubscribed = true; // mark subscription as true
+		}
 
 		updateData.isProfileCompleted = true;
 
